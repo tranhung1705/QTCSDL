413ce2aaf24a2f85f564c62a7e94df98f669a48c
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2021-06-13 20:56:15 +0700

    Create frmquanlichung.cs

---
+++ frmhoadon/quanlihoadon/frmquanlichung.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanlihoadon
+{
+    public partial class frmquanlichung : Form
+    {
+        public frmquanlichung()
+        {
+            InitializeComponent();
+        }
+
+        private void quảnLíHóaĐơnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmhoadon hoadon = new frmhoadon();
+            hoadon.MdiParent = this;
+            hoadon.Show();
+        }
+    }
+}
