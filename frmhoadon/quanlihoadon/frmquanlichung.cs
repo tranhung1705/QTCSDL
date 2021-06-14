19641fad3008f7835bc1ce5aeec090b1f9b31cfc
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2021-06-14 20:52:30 +0700

    Update frmquanlichung.cs

--- frmhoadon/quanlihoadon/frmquanlichung.cs
+++ frmhoadon/quanlihoadon/frmquanlichung.cs
@@ -23,5 +23,37 @@ namespace quanlihoadon
             hoadon.MdiParent = this;
             hoadon.Show();
         }
+
+        private void quảnLíNhàCungCấpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmnhacungcap nhacungcap = new frmnhacungcap();
+            nhacungcap.MdiParent = this;
+            nhacungcap.Show();
+        
+        }
+
+        private void quảnLíHàngHóaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmhang hang = new frmhang();
+            hang.MdiParent = this;
+            hang.Show();
+        }
+
+        private void thốngKêToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmthongke thongke = new frmthongke();
+            thongke.MdiParent = this;
+            thongke.Show();
+        }
+
+        private void frmquanlichung_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
     }
+
 }
