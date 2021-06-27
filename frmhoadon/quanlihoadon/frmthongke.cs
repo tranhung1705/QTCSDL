a6d023441b7eef9cf39e4a7b5018ce9d7a2807eb
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2021-06-27 22:44:52 +0700

    Update frmthongke.cs

--- frmhoadon/quanlihoadon/frmthongke.cs
+++ frmhoadon/quanlihoadon/frmthongke.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace quanlihoadon
 {
     public partial class frmthongke : Form
     {
+        string sCon = "Data Source=.\\sqlExpress;Initial Catalog=CUAHANG_TAPHOA;Integrated Security=True";
         public frmthongke()
         {
             InitializeComponent();
@@ -20,6 +22,38 @@ namespace quanlihoadon
         private void frmthongke_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnHienthi_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(sCon);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Xảy ra lỗi trong quá trình kết nối DB");
+            }
+            string sQuery = "select TenH,TenCC, SUM(SoLuongNhap)SoLuongNhap, SUM(ThanhTien)TongTien from HANG join HDNHAP_CHI_TIET on HANG.MaH = HDNHAP_CHI_TIET.MaH join HOADON_NHAP on HOADON_NHAP.MaHDN = HDNHAP_CHI_TIET.MAHDN join CUNG_CAP on CUNG_CAP.MaCC = HOADON_NHAP.MaCC where NgayNhap between @from and @to group by TenH, TenCC ";
+            string sFrom = date1.Value.ToString("yyyy-MM-dd");
+            string sTo = date2.Value.ToString("yyyy-MM-dd");
+            SqlCommand cmd = new SqlCommand(sQuery, con);
+            cmd.Parameters.AddWithValue("@from", sFrom);
+            cmd.Parameters.AddWithValue("@to", sTo);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                DataTable TK = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(TK);
+                dataGridView1.DataSource = TK;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            con.Close();
         }
     }
 }
