2cc7b45667c2eabbce5db5d1ec69bb572ad7dced
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2021-07-08 17:14:35 +0700

    Update frmnhacungcap.Designer.cs

--- frmhoadon/quanlihoadon/frmnhacungcap.Designer.cs
+++ frmhoadon/quanlihoadon/frmnhacungcap.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmnhacungcap));
             this.label1 = new System.Windows.Forms.Label();
             this.txtTimkiem = new System.Windows.Forms.TextBox();
             this.btnTimkiem = new System.Windows.Forms.Button();
@@ -241,6 +242,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmnhacungcap";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
